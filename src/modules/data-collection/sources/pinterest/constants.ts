57a7aff6e966897eaa5423799e4901c089a7445c
Abu Bakkar Siddique <abubakkar2502@gmail.com>
2026-07-09 23:13:13 +0530

    fix: remove 2025 filter from pinterest url

--- src/modules/data-collection/sources/pinterest/constants.ts
+++ src/modules/data-collection/sources/pinterest/constants.ts
@@ -1,13 +1,14 @@
-export const BASE_URL = 'https://in.pinterest.com';
+export const BASE_URL = "https://in.pinterest.com";
 
 export const SEARCH_URLS: Record<string, string> = {
-  kurtis2025: 'https://in.pinterest.com/search/pins/?q=kurti%20designs%202025&rs=typed',
+  kurtis2025:
+    "https://in.pinterest.com/search/pins/?q=kurti%20designs&rs=typed",
 };
 
 export const SCROLL_ITERATIONS = 8;
-export const SCROLL_DELAY_MS   = 2000;
+export const SCROLL_DELAY_MS = 2000;
 export const PAGE_LOAD_WAIT_MS = 3000;
-export const MODAL_TIMEOUT_MS  = 3000;
+export const MODAL_TIMEOUT_MS = 3000;
 
-export const HIGH_RES_SEGMENT  = '/736x/';
-export const LOW_RES_SEGMENT   = '/236x/';
+export const HIGH_RES_SEGMENT = "/736x/";
+export const LOW_RES_SEGMENT = "/236x/";
